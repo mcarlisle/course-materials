591ce4e4b643e2d538a4a5d618bb4bd0c1927e06
Michael Carlisle <6676184+mcarlisle@users.noreply.github.com>
2019-04-18 13:21:14 -0400

    small fix in OneTerm

--- Intro to Python/MTH3300_JMWA_HW07.py
+++ Intro to Python/MTH3300_JMWA_HW07.py
@@ -100,10 +100,8 @@ def OneTerm(coeff, degree):
         
     if coeff == 0.0:
         return "0"  # don't print anything else if coeff = 0
-    elif coeff == 1.0 and xStr != "":
-        return xStr # don't print coeff if coeff = 1 but x shows up
-    elif coeff == -1.0 and xStr != "":
-        return "-" + xStr  # don't print -1 if x should show up, just -x^n
+    elif coeff in {1.0, -1.0} and xStr != "":
+        return xStr # don't print coeff if coeff in {1,-1} and x shows up
     else:
         return str(coeff) + xStr  # usual look: a_n x^n
 
